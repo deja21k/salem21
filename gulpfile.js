b11e419b835cba4ab78339dc7b0ee96b82c7164a
Bojan Lazic <lazic@Bojans-MacBook-Pro.local>
2016-04-09 19:13:22 +0200

    gulp file added

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp'),
+  uglify = require('gulp-uglify'),
+  sass = require('gulp-ruby-sass'),
+  plumber = require('gulp-plumber'),
+  autoprefixer = require('gulp-autoprefixer'),
+  // livereload = require('gulp-livereload'),
+  combineMq = require('gulp-combine-mq');
+
+// Scripts Task
+gulp.task('scripts', function () {
+  gulp.src('assets/**/*.js')
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+// Styles Task
+gulp.task('sass', function () {
+  return sass('sass/**/*.scss', {
+    compass: true, sourcemap: true
+  })
+    .pipe(autoprefixer({
+      browsers: ['> 1%', 'IE 7'],
+      cascade: false
+    }))
+    .pipe(combineMq({
+      // beutify: false
+    }))
+    .pipe(plumber())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('dist/css'))
+});
+
+// Watches JS
+gulp.task('watch', function () {
+  gulp.watch('assets/**/*.js', ['scripts']);
+  gulp.watch('sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['scripts', 'sass', 'watch']);
