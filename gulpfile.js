ce7b4dbe903c89ca3236745dcfd0827d50688e55
Bojan Lazic <lazic@MacBook-Pro.local>
2016-04-11 23:46:01 +0200

    browser-sync insalled and added to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
   plumber = require('gulp-plumber'),
   autoprefixer = require('gulp-autoprefixer'),
   // livereload = require('gulp-livereload'),
-  combineMq = require('gulp-combine-mq');
+  combineMq = require('gulp-combine-mq')
+  browserSync = require('browser-sync').create();
 
 // Scripts Task
 gulp.task('scripts', function () {
@@ -17,7 +18,8 @@ gulp.task('scripts', function () {
 // Styles Task
 gulp.task('sass', function () {
   return sass('sass/**/*.sass', {
-    compass: true, sourcemap: true
+    compass: true,
+    sourcemap: true
   })
     .pipe(autoprefixer({
       browsers: ['> 1%', 'IE 7'],
@@ -29,12 +31,21 @@ gulp.task('sass', function () {
     .pipe(plumber())
     .on('error', sass.logError)
     .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
+    });
 });
 
 // Watches JS
 gulp.task('watch', function () {
   gulp.watch('js/**/*.js', ['scripts']);
-  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('sass/**/*.sass', ['sass']);
+  gulp.watch("/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['scripts', 'sass', 'watch']);
+gulp.task('default', ['scripts', 'sass', 'serve', 'watch']);
