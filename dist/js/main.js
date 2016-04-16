409001a9bb1b4f0ebf2925920ce26a9275b34be8
deja21k <thedoctor92@hotmail.com>
2016-04-16 12:57:58 +0200

    minor changes

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".slick--first").slick({infinite:!0})});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $('.slick--first').slick({
-    setting-name: setting-value
+    infinite: true
   });
 });
